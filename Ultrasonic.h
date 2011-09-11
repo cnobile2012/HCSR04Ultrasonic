ed6dafc4ee4b5bab41dfbd5901e8de38d27ad7c3
cnobile <cnobile@ed0607b5-9f2d-40cd-ad1e-287a16c2e87a>
2011-09-11 20:50:38 +0000

    Initial Checkin
    git-svn-id: file:///exports/nas-storage/cnobile/repos/svnroot/arduino-svn/HCSR04Ultrasonic/trunk@2 ed0607b5-9f2d-40cd-ad1e-287a16c2e87a

---
+++ Ultrasonic.h
@@ -0,0 +1,60 @@
+/*
+ * Ultrasonic.h - Library for HR-SC04 Ultrasonic Ranging Module.
+ *
+ * Ideas from:
+ *   Created by ITead studio. Alex, Apr 20, 2010.
+ *   iteadstudio.com
+ *
+ * SVN Keywords
+ * ----------------------------------
+ * $Author: $
+ * $Date: $
+ * $Revision: $
+ * ----------------------------------
+ */
+
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#include <stddef.h>
+#include <WProgram.h>
+
+// Undefine COMPILE_STD_DEV if you don't want Standard Deviation.
+#define COMPILE_STD_DEV
+
+
+typedef struct bufferCtl
+    {
+    float *pBegin;
+    float *pIndex;
+    size_t length;
+    bool filled;
+    } BufCtl;
+
+class Ultrasonic
+    {
+    public:
+    Ultrasonic(int tp, int ep);
+    long timing();
+    long convert(long microsec, int metric);
+    static const int IN = 0;
+    static const int CM = 1;
+
+#ifdef COMPILE_STD_DEV
+    bool sampleCreate(size_t size, ...);
+    void sampleClear();
+    float unbiasedStdDev(long msec, size_t bufNum);
+#endif // COMPILE_STD_DEV
+
+    private:
+    int _trigPin;
+    int _echoPin;
+
+#ifdef COMPILE_STD_DEV
+    size_t _numBufs;
+    BufCtl *_pBuffers;
+    void _sampleUpdate(BufCtl *buf, float msec);
+#endif // COMPILE_STD_DEV
+    };
+
+#endif // ULTRASONIC_H
